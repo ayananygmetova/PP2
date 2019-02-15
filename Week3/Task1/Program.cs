6d74d3a3868b00c367bae589fab77f6e694b17fb
Ayana <ayananyfmetova@gmail.com>
2019-02-16 00:51:12 +0600

    fixed far manager and added comments

--- Week3/Task1/Program.cs
+++ Week3/Task1/Program.cs
@@ -7,11 +7,14 @@ using System.IO;
 
 namespace Task1
 {
-        class FarManager {
-        string path;
-        int cursor,sz;
-        bool ok;
-        
+    class FarManager
+    {
+        public string path;
+        public int cursor, sz;
+        public bool ok;
+        DirectoryInfo directory = null;
+        FileSystemInfo currentFs = null;
+
         public FarManager(string path)
         {
             this.path = path;
@@ -22,46 +25,61 @@ namespace Task1
         public void Color(FileSystemInfo fs, int index)
         {
             //Colour of cursor
-            if (cursor == index) 
+            if (cursor == index)
             {
                 Console.BackgroundColor = ConsoleColor.White;
                 Console.ForegroundColor = ConsoleColor.Black;
-
+                currentFs = fs; // to remember where is cursor 
             }
-            else if (fs.GetType() == typeof(DirectoryInfo))
+            else if (fs.GetType() == typeof(DirectoryInfo)) //coloring directories
             {
                 Console.BackgroundColor = ConsoleColor.Blue;
                 Console.ForegroundColor = ConsoleColor.White;
             }
-            if (fs.GetType() == typeof(FileInfo))
+            else if (fs.GetType() == typeof(FileInfo))
             {
-                Console.BackgroundColor = ConsoleColor.Green;
+                Console.BackgroundColor = ConsoleColor.Green; //coloring files
                 Console.ForegroundColor = ConsoleColor.Yellow;
             }
         }
 
+        //Function which allows to see folders and files
         public void Show()
         {
             Console.BackgroundColor = ConsoleColor.Black;
             Console.Clear();
-            DirectoryInfo directory = new DirectoryInfo(path);
+            directory = new DirectoryInfo(path);
             //folder which has folders and files(PP2)
             FileSystemInfo[] fs = directory.GetFileSystemInfos();
-            sz = fs.Length;
+            //array of folders only
+            FileSystemInfo[] fs1 = directory.GetDirectories();
+            //array of files only
+            FileSystemInfo[] fs2 = directory.GetFiles();
+            sz = fs1.Length + fs2.Length; //unit lenght
+            DirectoryInfo[] dir = directory.GetDirectories();
             //Print all folders and files
-            for (int i = 0, k = 0; i < fs.Length; i++)
+            //k - is the index of cursor indepent from hidden or unblock directory
+            int h = 0; //index for files
+            for (int i = 0, k = 0; i < fs1.Length; i++)
             {
-                if (ok == false && fs[i].Name.StartsWith("."))
-                {
+                if (ok == false && fs1[i].Name.StartsWith("."))
                     continue;
-                }
-                Color(fs[i], i);
-                Console.WriteLine((fs[i].Name));
+                Color(fs1[i], k);
+                Console.WriteLine(k + 1 + ". " + fs1[i]);
                 k++;
+                h++;
+            }
+            for (int i = 0; i < fs2.Length; i++)
+            {
+                if (ok == false && fs2[i].Name.StartsWith("."))
+                    continue;
+                Color(fs2[i], h);
+                Console.WriteLine(h + 1 + ". " + fs2[i]);
+                h++;
             }
         }
-      
-        
+
+
         // Moving cursor
         public void Up()
         {
@@ -75,12 +93,30 @@ namespace Task1
             if (cursor >= sz)
                 cursor = 0;
         }
+
+
+        //Calculating of size, which depends on hidden folders
+        public void CalcSize()
+        {
+            directory = new DirectoryInfo(path);
+            FileSystemInfo[] fs = directory.GetFileSystemInfos();
+            sz = fs.Length;
+            if (ok == false)
+                for (int i = 0; i < fs.Length; i++)
+                    if (fs[i].Name[0] == '.')
+                        sz--;                   //decrease the size
+
+        }
+
+
+        //main function, where all actions happen
         public void Start()
         {
             ConsoleKeyInfo consoleKey = Console.ReadKey();
-            while(consoleKey.Key != ConsoleKey.Escape)
+            while (consoleKey.Key != ConsoleKey.Escape)
             {
                 Show();
+                CalcSize();
                 consoleKey = Console.ReadKey();
                 if (consoleKey.Key == ConsoleKey.UpArrow)
                     Up();
@@ -89,11 +125,66 @@ namespace Task1
                 if (consoleKey.Key == ConsoleKey.RightArrow)
                 {
                     ok = false;
-                    
+                    cursor = 0;
                 }
-                    
                 if (consoleKey.Key == ConsoleKey.LeftArrow)
-                    ok = true; 
+                {
+                    cursor = 0;
+                    ok = true;
+                }
+                if (consoleKey.Key == ConsoleKey.Enter)
+                {
+                    if (currentFs.GetType() == typeof(DirectoryInfo))     //to open directories
+                    {
+                        cursor = 0;
+                        path = currentFs.FullName;
+
+                    }
+                    if (currentFs.GetType() == typeof(FileInfo))        //to open files and read
+                    {
+                        StreamReader st = new StreamReader(currentFs.FullName);
+                        Console.WriteLine(st.ReadToEnd());
+                        st.Close();
+                        Console.ReadKey();
+                        Console.Clear();
+                    }
+                }
+                if (consoleKey.Key == ConsoleKey.Backspace)         //returns back
+                {
+                    cursor = 0;
+                    path = directory.Parent.FullName;
+                }
+                if (consoleKey.Key == ConsoleKey.D)                 //delete files or directories
+                {
+                    if (currentFs.GetType() == typeof(FileInfo))
+                    {
+                        FileInfo fi = new FileInfo(currentFs.FullName);
+                        fi.Delete();
+                    }
+                    if (currentFs.GetType() == typeof(DirectoryInfo))
+                    {
+                        DirectoryInfo di = new DirectoryInfo(currentFs.FullName);
+                        di.Delete(true);
+                    }
+                }
+                if (consoleKey.Key == ConsoleKey.R)                 //renames folders of files
+                {
+                    if (currentFs.GetType() == typeof(FileInfo))
+                    {
+                        Console.BackgroundColor = ConsoleColor.Black;
+                        Console.Clear();
+                        Console.ForegroundColor = ConsoleColor.White;
+                        string fiName = Console.ReadLine();
+                        File.Move(currentFs.FullName, Path.GetDirectoryName(currentFs.FullName) + "/" + fiName);
+                    }
+
+                    if (currentFs.GetType() == typeof(DirectoryInfo))
+                    {
+                        Console.BackgroundColor = ConsoleColor.Black;
+                        Console.Clear();
+                        string newdi = Console.ReadLine();
+                        Directory.Move(currentFs.FullName, Path.GetDirectoryName(currentFs.FullName) + "/" + newdi);
+                    }
                 }
             }
         }
@@ -101,10 +192,11 @@ namespace Task1
 
     class Program
     {
-        static void Main(string[] args) {
+        static void Main(string[] args)
+        {
             string path = "C:/Users/Ayana/Documents/PP2";
-        Task1.FarManager farManager = new Task1.FarManager(path);
-        farManager.Start();
+            Task1.FarManager farManager = new Task1.FarManager(path);
+            farManager.Start();
         }
     }
-
+}
