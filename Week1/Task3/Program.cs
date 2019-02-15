45ebe9887e732ebe7d9a596123d184059857732f
Ayana <ayananyfmetova@gmail.com>
2019-02-16 01:06:37 +0600

    method for double elements of array, didn't know about it before

--- Week1/Task3/Program.cs
+++ Week1/Task3/Program.cs
@@ -14,10 +14,20 @@ namespace Task3
             //string of nums convert in int type && elements of array
             for (int i = 0; i < n; i++)
                 a[i] = int.Parse(s[i]);
-
+            
+            //repeated elements of array
+            int[] a2 = new int[2 * n];
+            int k = 0;
+            for (int i=0; i<n; i++)
+            {
+                a2[k] = a[i];
+                k++;
+                a2[k] = a[i];
+                k++;
+            }
             //print 
-            for (int i = 0; i < n; i++)
-                Console.Write(a[i] + " " + a[i] + " ");
+            for (int i = 0; i < a2.Length; i++)
+                Console.Write(a2[i]  + " ");
 
             Console.ReadKey();
         }
